b3e0b73f160526b7f0f74f7519ac798661a389b6
Ilias Rinis <iliasr@gmail.com>
2023-05-29 14:10:28 +0300

    Paginate over all repos

--- pkg/github/lang.go
+++ pkg/github/lang.go
@@ -7,9 +7,21 @@ import (
 	"github.com/liouk/gh-stats/pkg/icons"
 	"github.com/liouk/gh-stats/pkg/log"
 	"github.com/liouk/gh-stats/pkg/stats"
+	"github.com/shurcooL/githubv4"
 )
 
 type set map[string]struct{}
+
+func (s set) AddAll(slice []string) {
+	for _, elem := range slice {
+		s[strings.ToLower(elem)] = struct{}{}
+	}
+}
+
+func (s set) ContainsIgnoreCase(elem string) bool {
+	_, ok := s[strings.ToLower(elem)]
+	return ok
+}
 
 func (c *AuthenticatedGitHubContext) LangStats(maxNum int, ignore []string) ([]*stats.Lang, error) {
 	var langQuery struct {
@@ -26,13 +38,12 @@ func (c *AuthenticatedGitHubContext) LangStats(maxNum int, ignore []string) ([]*
 						}
 					} `graphql:"languages(first: 100)"`
 				}
-			} `graphql:"repositories(first: 100, ownerAffiliations: OWNER, isFork: false, privacy: PUBLIC)"`
+				PageInfo struct {
+					HasNextPage bool
+					EndCursor   githubv4.String
+				}
+			} `graphql:"repositories(first: 100, after: $after, ownerAffiliations: OWNER, isFork: false, privacy: PUBLIC)"`
 		}
-	}
-
-	err := c.githubClient.Query(c.ctx, &langQuery, nil)
-	if err != nil {
-		return nil, err
 	}
 
 	ignoreSet := &set{}
@@ -40,21 +51,39 @@ func (c *AuthenticatedGitHubContext) LangStats(maxNum int, ignore []string) ([]*
 
 	bytes := map[string]int{}
 	totalBytes := 0
-	for _, repo := range langQuery.Viewer.Repositories.Nodes {
-		log.Logvf("%s%s\n", icons.Repo, repo.NameWithOwner)
-		for _, lang := range repo.Languages.Edges {
-			if ignoreSet.ContainsIgnoreCase(lang.Node.Name) {
-				continue
-			}
 
-			n := lang.Node.Name
-			if _, ok := bytes[n]; !ok {
-				bytes[n] = 0
-			}
-			bytes[n] += lang.Size
-			totalBytes += lang.Size
-			log.Logvf("  %s: %d\n", n, lang.Size)
+	vars := map[string]interface{}{
+		"after": (*githubv4.String)(nil),
+	}
+
+	for {
+		err := c.githubClient.Query(c.ctx, &langQuery, vars)
+		if err != nil {
+			return nil, err
 		}
+
+		for _, repo := range langQuery.Viewer.Repositories.Nodes {
+			log.Logvf("%s%s\n", icons.Repo, repo.NameWithOwner)
+			for _, lang := range repo.Languages.Edges {
+				if ignoreSet.ContainsIgnoreCase(lang.Node.Name) {
+					continue
+				}
+
+				n := lang.Node.Name
+				if _, ok := bytes[n]; !ok {
+					bytes[n] = 0
+				}
+				bytes[n] += lang.Size
+				totalBytes += lang.Size
+				log.Logvf("  %s: %d\n", n, lang.Size)
+			}
+		}
+
+		if !langQuery.Viewer.Repositories.PageInfo.HasNextPage {
+			break
+		}
+
+		vars["after"] = langQuery.Viewer.Repositories.PageInfo.EndCursor
 	}
 
 	lang := make([]*stats.Lang, 0)
@@ -71,15 +100,4 @@ func (c *AuthenticatedGitHubContext) LangStats(maxNum int, ignore []string) ([]*
 		return lang[i].Perc > lang[j].Perc
 	})
 	return lang[0:lim], nil
-}
-
-func (s set) AddAll(slice []string) {
-	for _, elem := range slice {
-		s[strings.ToLower(elem)] = struct{}{}
-	}
-}
-
-func (s set) ContainsIgnoreCase(elem string) bool {
-	_, ok := s[strings.ToLower(elem)]
-	return ok
 }
